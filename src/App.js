31f7a0d471e35bf4a2cd48420ef49c727c0f3ff7
Thato Mmatli (ZA) <Thato.Mmatli@absa.africa>
2022-11-10 11:36:39 +0200

    adding modal

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 
-import { Fragment} from 'react'
+import { Fragment, useState} from 'react'
 import Header from './components/Layout/Header'
 import Meals from './components/Meals/Meals'
-
+import Cart from './components/Cart/Cart'
 
 
 function App() {
+
+const [cartIsShown, setCartIShown] =  useState(false);
+
+const showCartHandler = () => {
+  setCartIShown(true);
+}
+
+
+const hideCartHandler = () => {
+  setCartIShown(false)
+}
+
+
   return (
     <Fragment>
-     <Header />
+   { cartIsShown &&  <Cart />   }
+     <Header onShowCart={showCartHandler} />
 <main>
 <Meals />
 </main>
--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -4,7 +4,7 @@ import classes from './HeaderCartButton.module.css';
 const HeaderCartButton = props => {
 
 
-return <button  className={classes.button}>
+return ( <button  className={classes.button}  onClick={props.onClick}>
 <span className={classes.icon}>
 <CartIcon />
 
@@ -12,7 +12,7 @@ return <button  className={classes.button}>
 <span> Your Cart </span>
 <span  className={classes.badge}> 3</span>
 </button>
-
+);
 };
 
 export default HeaderCartButton;
